2be22187e1ccec48b4610f16be496ab9e04becf0
Atul <atulreso1@gmail.com>
2022-03-28 10:10:15 +0530

    curring n

--- Interview/Currying/curryClosures.js
+++ Interview/Currying/curryClosures.js
@@ -11,3 +11,7 @@ multiplyByThree(03);
 
 let multiplyByFour = multiply(022);
 multiplyByFour(04);
+
+// Can you write code for this function: sum(a)(b)©….( n)(). This should return the sum of all the numbers a+b+c+..+n.;:
+const sum = (a) => (b) => (c) => (d) => (e) => () => a + b + c + d + e;
+console.log(sum(1)(2)(3)(4)(5)());
