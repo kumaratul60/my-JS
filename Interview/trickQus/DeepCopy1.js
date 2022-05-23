4fa074f8ee32264adffa2276fbd931d6f710172b
Atul <atulreso1@gmail.com>
2022-05-23 12:56:47 +0530

    add some switch changes jsonVSStriglify and other

--- Interview/trickQus/DeepCopy1.js
+++ Interview/trickQus/DeepCopy1.js
@@ -2,6 +2,8 @@
 Unlike the shallow copy, deep copy makes a copy of all the members of the old object, allocates separate memory location for the new object and then assigns the copied members to the new object. In this way, both the objects are independent of each other and in case of any modification to either one the other is not affected. Also, if one of the objects is deleted the other still remains in the memory. Now to create a deep copy of an object in JavaScript we use JSON.parse() and JSON.stringify() methods.
 */
 
+// Deep copy is a true copy for nested objects We can’t change the original object in the deep clone since Lodash copies properties recursively and there is no data loss.
+
 var employee = {
   eid: "E102",
   ename: "Jack",
